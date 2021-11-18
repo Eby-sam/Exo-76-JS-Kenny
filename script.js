9f18e792071032b9d05f3e1c3d3d7a1a1433e94b
coque <coquelet.samuel@gmail.com>
2021-11-18 13:22:24 +0100

    fini++

--- script.js
+++ script.js
@@ -1,50 +1,24 @@
-let t = parseInt(document.getElementById('kenny').style.top);
-let l = parseInt(document.getElementById('kenny').style.left);
-let kenny = document.getElementById('kenny');
-
 document.getElementById('up').addEventListener('click',function() {
-    if (t === 0){
-        kenny.style.top = '0px';
-    }
-    else {
-        t = t - 10;
-        kenny.style.top = t + 'px';
-    }
-});
-
-document.getElementById('down').addEventListener('click',function() {
-    if (t === 470) {
-        kenny.style.top = '470px';
-    }
-    else {
-        t = t + 10;
-        kenny.style.top = t + 'px';
-    }
+    let t = parseInt(document.getElementById('kenny').style.top);
+    t = t - 10;
+    document.getElementById('kenny').style.top = t + 'px';
 });
 
 document.getElementById('left').addEventListener('click',function() {
-    if (l === 60) {
-        alert('Kenny est mort');
-        kenny.style.left = '200px';
-        kenny.style.top = '200px';
-    }
-    else {
-        l = l - 10;
-        kenny.style.left = l + 'px';
-    }
+    let l = parseInt(document.getElementById('kenny').style.left);
+    l = l - 10;
+    document.getElementById('kenny').style.left = l + 'px';
 });
 
 document.getElementById('right').addEventListener('click',function() {
-    if (l === 470) {
-        kenny.style.left = '470px';
-    }
-    else {
-        l = l + 10;
-        kenny.style.left = l + 'px';
-    }
+    let r = parseInt(document.getElementById('kenny').style.left);
+    r = r + 10;
+    document.getElementById('kenny').style.left = r + 'px';
 });
 
-
-let viewport = document.getElementById('viewport');
-
+document.getElementById('down').addEventListener('click',function() {
+    let d = parseInt(document.getElementById('kenny').style.top);
+    d = d + 10;
+    document.getElementById('kenny').style.top = d + 'px';
+});
 
