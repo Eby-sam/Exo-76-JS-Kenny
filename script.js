4a5d48539b218cde2b705f984a81eadf37da03e1
coque <coquelet.samuel@gmail.com>
2021-11-18 21:05:25 +0100

    hoooo mon dieu , ils ont tué Kenny ......

--- script.js
+++ script.js
@@ -1,24 +1,120 @@
+let t = parseInt(document.getElementById('kenny').style.top);
+let l = parseInt(document.getElementById('kenny').style.left);
+let kenny = document.getElementById('kenny');
+let i = 0;
+
+// pade up
 document.getElementById('up').addEventListener('click',function() {
-    let t = parseInt(document.getElementById('kenny').style.top);
-    t = t - 10;
-    document.getElementById('kenny').style.top = t + 'px';
+    if (t === 0){
+        kenny.style.top = '0px';
+    }
+    else {
+        t = t - 10;
+        kenny.style.top = t + 'px';
+    }
 });
 
-document.getElementById('left').addEventListener('click',function() {
-    let l = parseInt(document.getElementById('kenny').style.left);
-    l = l - 10;
-    document.getElementById('kenny').style.left = l + 'px';
-});
-
-document.getElementById('right').addEventListener('click',function() {
-    let r = parseInt(document.getElementById('kenny').style.left);
-    r = r + 10;
-    document.getElementById('kenny').style.left = r + 'px';
-});
-
+// pade down
 document.getElementById('down').addEventListener('click',function() {
-    let d = parseInt(document.getElementById('kenny').style.top);
-    d = d + 10;
-    document.getElementById('kenny').style.top = d + 'px';
+    if (t === 470) {
+        kenny.style.top = '470px';
+    }
+    else {
+        t = t + 10;
+        kenny.style.top = t + 'px';
+    }
 });
 
+// pade left
+document.getElementById('left').addEventListener('click',function() {
+    if (l === 60) {
+        alert('Kenny est mort');
+        kenny.style.top = '200px';
+        kenny.style.left = '200px';
+        l = 200;
+        i++
+        document.getElementById('i').innerText = i.toString();
+    }
+    else {
+        l = l - 10;
+        kenny.style.left = l + 'px';
+    }
+});
+
+// pade right
+document.getElementById('right').addEventListener('click',function() {
+    if (l === 470) {
+        kenny.style.left = '470px';
+    }
+    else {
+        l = l + 10;
+        kenny.style.left = l + 'px';
+    }
+});
+
+// button up
+function up() {
+    if (t === 0){
+        kenny.style.top = '0px';
+    }
+    else {
+        t = t - 10;
+        kenny.style.top = t + 'px';
+    }
+}
+
+// button down
+function down() {
+    if (t === 470){
+        kenny.style.top = '470px';
+    }
+    else {
+        t = t + 10;
+        kenny.style.top = t + 'px';
+    }
+}
+
+// button left
+function left() {
+    if (l === 60) {
+        alert('Kenny est mort');
+        kenny.style.top = '200px';
+        kenny.style.left = '200px';
+        l = 200;
+        i++
+        document.getElementById('i').innerText = i.toString();
+    }
+    else {
+        l = l - 10;
+        kenny.style.left = l + 'px';
+    }
+}
+
+// button right
+function  right() {
+    if (l === 470) {
+        kenny.style.left = '470px';
+    }
+    else {
+        l = l + 10;
+        kenny.style.left = l + 'px';
+    }
+}
+
+document.body.addEventListener("keydown", function (e){
+    let keyPressed = e.key;
+    switch (keyPressed) {
+        case 'ArrowLeft':
+            left();
+            break;
+        case 'ArrowUp':
+            up();
+            break;
+        case 'ArrowRight':
+            right();
+            break;
+        case  'ArrowDown':
+            down();
+            break;
+    }
+});
